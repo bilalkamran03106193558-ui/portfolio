1111f4e0d0616b8e0f443213d3d38e0c50f047d6
BTrex <bilalkamran03106193558@gmail.com>
2026-08-26 00:16:30 +0500

    Fix page opening position and improve scroll effects

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+// Always open website from the top
+window.onload = function(){
+    window.scrollTo(0,0);
+};
+
+// Scroll animation
 window.addEventListener('scroll',()=>{
- const items=document.querySelectorAll('.card,.project');
+ const items=document.querySelectorAll('.card,.project,.about-box');
  items.forEach(item=>{
   const position=item.getBoundingClientRect().top;
   if(position < window.innerHeight-100){
