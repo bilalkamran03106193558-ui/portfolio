0ea7e6e1ddd7f49c5a439bf12c5853de042edb53
BTrex <bilalkamran03106193558@gmail.com>
2026-08-25 23:30:59 +0500

    Add portfolio website JavaScript effects

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+window.addEventListener('scroll',()=>{
+ const items=document.querySelectorAll('.card,.project');
+ items.forEach(item=>{
+  const position=item.getBoundingClientRect().top;
+  if(position < window.innerHeight-100){
+   item.style.opacity='1';
+   item.style.transform='translateY(0)';
+  }
+ });
+});
